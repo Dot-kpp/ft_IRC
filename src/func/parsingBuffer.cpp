4adb5696c58f3cfab98f83044c3e34270d09661e
Alexandre Couture <acouture@c1r7p3.42quebec.com>
2023-11-22 17:16:10 -0500

    mege with dev

--- src/func/parsingBuffer.cpp
+++ src/func/parsingBuffer.cpp
@@ -1,25 +1,5 @@
 #include "../../inc/server/Server.hpp"
 
-// Numeric Replies:
-
-// ERR_NONICKNAMEGIVEN (431)
-// ERR_ERRONEUSNICKNAME (432)
-// ERR_NICKNAMEINUSE (433)
-// ERR_NICKCOLLISION (436)
-// f the server does not accept the new nickname supplied by the client as valid (for instance, due to containing invalid characters), it should issue an ERR_ERRONEUSNICKNAME numeric and ignore the NICK command. Servers MUST allow at least all alphanumerical characters, square and curly brackets ([]{}), backslashes (\), and pipe (|) characters in nicknames, and MAY disallow digits as the first character. Servers MAY allow extra characters, as long as they do not introduce ambiguity in other commands, including:
-
-// no leading # character or other character advertized in CHANTYPES
-// no leading colon (:)
-// no ASCII space
-// If the server does not receive the <nickname> parameter with the NICK command, it should issue an ERR_NONICKNAMEGIVEN numeric and ignore the NICK command.
-
-// The NICK message may be sent from the server to clients to acknowledge their NICK command was successful, and to inform other clients about the change of nickname. In these cases, the <source> of the message will be the old nickname [ [ "!" user ] "@" host ] of the user who is changing their nickname.
-
-//   CHANTYPES=#
-
-//   CHANTYPES=&#
-
-//   CHANTYPES=#&
 bool parseNickname(std::string nickname, int clientFd)
 {
     Server *server = Server::instance;
