32753a96353187087ea8bd6ca49c7ed51e7bd453
Alexandre Couture <acouture@c1r1p3.42quebec.com>
2023-11-23 14:30:07 -0500

    creating classes

---
+++ inc/commands/Command.hpp
@@ -0,0 +1,19 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+
+class Command
+{
+    private:
+        Command();
+
+    public:
+        
+};
+#endif
+
