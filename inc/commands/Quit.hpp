a31e76cacdb8f6d36b7e936966e82b0a80493318
Alexandre Couture <acouture@c1r7p2.42quebec.com>
2023-11-29 15:18:11 -0500

    added quit

---
+++ inc/commands/Quit.hpp
@@ -0,0 +1,18 @@
+
+#ifndef QUIT_HPP
+#define QUIT_HPP
+
+#include "Command.hpp"
+
+class Quit : public Command
+{
+private:
+    std::string command;
+public:
+    Quit();
+    virtual ~Quit();
+    
+    virtual bool execute(Server *server, std::string args, int clientFd);
+};
+
+#endif // QUIT_HPP
