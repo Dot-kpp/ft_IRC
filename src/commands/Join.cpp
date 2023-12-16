478fd1619b09c35154372ba577b227822a761d57
Clmntsnd <clement.senand@gmail.com>
2023-12-16 14:31:19 -0500

    sanitize JOIN

--- src/commands/Join.cpp
+++ src/commands/Join.cpp
@@ -14,9 +14,7 @@ bool compareChannelName(const Channels& obj, const std::string& channelName) {
 
 bool Join::execute(Server *server, std::string args, int clientFd) {
 	if (args.empty() || clientFd < 0) {
-		std::string replyError =
-				":" + server->getServerName() + " 461 " + server->clients[clientFd].getNickName() + " JOIN " +
-				" :No such channel \r\n";
+		std::string replyError = ":" + server->getServerName() + " 461 " + server->clients[clientFd].getNickName() + " JOIN " +	" :No such channel \r\n";
 		send(clientFd, replyError.c_str(), replyError.size(), 0);
 		return false;
 	}
@@ -32,7 +30,6 @@ bool Join::execute(Server *server, std::string args, int clientFd) {
 	std::string channelName;
 	std::istringstream iss(args);
 
-//	iss >> channelName;
 	while (std::getline(iss, channelName, ',')) {
 		if (channelName.front() != '#') {
 			std::string replyError = ":" + server->getServerName() + " 476 " + channelName + " :Bad Channel Mask\r\n";
@@ -42,73 +39,61 @@ bool Join::execute(Server *server, std::string args, int clientFd) {
 		// check if there are # after the first position of the string
 		if (channelName.find('#', 1) != std::string::npos)
 			return false;
+
 		Channels *channel = server->getChannelByName(channelName);
+
 		if (channel == nullptr) {
 			// Channel does not exist, create it
-			Channels newChannel; // Assuming Channels is a class
-			newChannel.addUsers(client, 1); // Pass the Client object and role ID
-			newChannel.setChannelName(channelName); // Set the name of the channel
-			server->channel.push_back(newChannel); // Add the channel to the server's channel list
+			Channels newChannel;
+			newChannel.addUsers(client, 1);
+			newChannel.setChannelName(channelName);
+			server->channel.push_back(newChannel);
+
 			std::string reply = ":" + server->clients[clientFd].getNickName() + " JOIN " + channelName + "\r\n";
 			send(clientFd, reply.c_str(), reply.size(), 0);
 
-			std::string operatorMessage =
-					":" + server->getServerName() + " 381 " + server->clients[clientFd].getNickName() +
-					" :You are now an IRC operator \r\n";
+			std::string operatorMessage = ":" + server->getServerName() + " 381 " + server->clients[clientFd].getNickName() + " :You are now an IRC operator \r\n";
 			send(clientFd, operatorMessage.c_str(), operatorMessage.size(), 0);
 
-			std::cout << "Channel " << channelName << " created and client " << server->clients[clientFd].getNickName()
-					  << " added to it." << std::endl;
+			std::cout << "Channel " << channelName << " created and client " << server->clients[clientFd].getNickName() << " added to it." << std::endl;
 		} else {
 			if (channel->getInviteOnly() && !channel->isUserInvited(client)) {
 				// Channel is invite-only and user is not invited
-				std::string replyError =
-						":" + server->getServerName() + " 473 " + server->clients[clientFd].getNickName() + " " +
-						channelName + " :Cannot join channel (+i) - not invited \r\n";
+				std::string replyError = ":" + server->getServerName() + " 473 " + server->clients[clientFd].getNickName() + " " + channelName + " :Cannot join channel (+i) - not invited \r\n";
 				send(clientFd, replyError.c_str(), replyError.size(), 0);
 				return false;
 			}
 			if (channel->hasUser(client)) {
 				// User is already in the channel
-				std::string replyError =
-						":" + server->getServerName() + " 443 " + server->clients[clientFd].getNickName() + " " +
-						channelName + " :is already on channel\r\n";
+				std::string replyError = ":" + server->getServerName() + " 443 " + server->clients[clientFd].getNickName() + " " + channelName + " :is already on channel\r\n";
 				send(clientFd, replyError.c_str(), replyError.size(), 0);
 				return false;
 			}
 			if (channel->getHasKey()) {
-				// Channel has a key, check if the user provided the correct key
 				std::string providedKey;
 				iss >> providedKey;
 
 				if (providedKey != channel->getKey()) {
-					// Incorrect key provided
 					std::string replyError =
-							":" + server->getServerName() + " 475 " + server->clients[clientFd].getNickName() + " " +
-							channelName + " :Cannot join channel (+k) - bad key \r\n";
+							":" + server->getServerName() + " 475 " + server->clients[clientFd].getNickName() + " " + channelName + " :Cannot join channel (+k) - bad key \r\n";
 					send(clientFd, replyError.c_str(), replyError.size(), 0);
 					return false;
 				}
 			}
 			if (channel->getUserLimitValue() == channel->getUsers().size() && channel->getHasLimit()) {
-				// Channel is full
-				std::string replyError =
-						":" + server->getServerName() + " 471 " + server->clients[clientFd].getNickName() + " " +
-						channelName + " :Cannot join channel (+l) - channel is full \r\n";
+				std::string replyError = ":" + server->getServerName() + " 471 " + server->clients[clientFd].getNickName() + " " + channelName + " :Cannot join channel (+l) - channel is full \r\n";
 				send(clientFd, replyError.c_str(), replyError.size(), 0);
 				return false;
 			}
+
 			// Channel exists, add the client to it
 			channel->addUsers(client, 2);
 
 			// Send the JOIN message to the other clients in the channel
 			std::string reply = ":" + server->clients[clientFd].getNickName() + " JOIN " + channelName + "\r\n";
 			send(clientFd, reply.c_str(), reply.size(), 0);
-
-			std::cout << "Client " << clientFd << " added to the existing channel " << channelName << std::endl;
-
-			//need to modify this
 			server->broadcastToChannel(channel->getChannelName(), reply, clientFd, client->getNickName());
+			std::cout << "Client " << clientFd << " added to the existing channel " << channelName << std::endl;
 		}
 	}
 	return true;
